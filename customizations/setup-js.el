8cd5ec052ea88eb5f724cf5ec459e2e87db2b3a2
Matthew Hoselton <matthew@periscopic.com>
2017-01-26 16:37:30 -0800

    disable sass/scss-sass-lint

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -32,6 +32,7 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq-default flycheck-disabled-checkers '(javascript-jscs))
+(setq-default flycheck-disabled-checkers '(sass/scss-sass-lint))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 
 ;; typescript
