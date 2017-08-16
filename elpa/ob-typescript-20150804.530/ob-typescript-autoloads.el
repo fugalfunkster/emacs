8a1048122bc2420d11f388ff1a7b638e32415af7
Matthew Hoselton <matthew@periscopic.com>
2017-08-16 10:48:07 -0700

    add elpa files in prep for update

---
+++ elpa/ob-typescript-20150804.530/ob-typescript-autoloads.el
@@ -0,0 +1,16 @@
+;;; ob-typescript-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ob-typescript.el") (22562 20393 82951
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ob-typescript-autoloads.el ends here
