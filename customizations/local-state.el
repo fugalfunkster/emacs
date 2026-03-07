b4db1b4d06b936c3cab0c5deeb9bb4385965b099
Matthew Roost <matthew@joinroost.com>
2026-03-07 11:29:24 -0800

    rm packages

---
+++ customizations/local-state.el
@@ -0,0 +1,34 @@
+;; Local State
+
+;; Keep volatile state off Dropbox to avoid sync churn and conflicted copies.
+
+
+(defconst my-local-emacs-dir (expand-file-name "~/.emacs.d-local/"))
+(unless (file-directory-p my-local-emacs-dir)
+  (make-directory my-local-emacs-dir t))
+
+(defun my-local-emacs-file (name)
+  (expand-file-name name my-local-emacs-dir))
+
+(dolist (dir (list (my-local-emacs-file "backups")
+                   (my-local-emacs-file "auto-save")
+                   (my-local-emacs-file "auto-save-list")
+                   (my-local-emacs-file "url")
+                   (my-local-emacs-file "transient")))
+  (unless (file-directory-p dir)
+    (make-directory dir t)))
+
+(setq recentf-save-file (my-local-emacs-file ".recentf"))
+(setq smex-save-file (my-local-emacs-file ".smex-items"))
+(setq save-place-file (my-local-emacs-file "places"))
+(setq projectile-known-projects-file (my-local-emacs-file "projectile-bookmarks.eld"))
+(setq package-user-dir (my-local-emacs-file "elpa/"))
+(setq custom-file (my-local-emacs-file "custom.el"))
+(setq forge-database-file (my-local-emacs-file "forge-database.sqlite"))
+
+(setq backup-directory-alist `(("." . ,(my-local-emacs-file "backups"))))
+(setq auto-save-file-name-transforms `((".*" ,(my-local-emacs-file "auto-save/") t)))
+(setq auto-save-list-file-prefix (my-local-emacs-file "auto-save-list/.saves-"))
+
+(setq url-configuration-directory (my-local-emacs-file "url/"))
+(setq transient-history-file (my-local-emacs-file "transient/history.el"))
